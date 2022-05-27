15770341d888dafafdaab89f10c76ef3294f9d10
wengru <573519534@qq.com>
2022-05-28 02:31:14 +0800

    feat: 208字典树解法

--- src/Trie.java
+++ src/Trie.java
@@ -1,19 +1,52 @@
 class Trie {
 
-    public Trie() {
+    // 另一种解法，二维数组
 
+    class TrieNode {
+        boolean end = false;
+        TrieNode[] nodes = new TrieNode[26];
+    }
+
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
     }
 
     public void insert(String word) {
-
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if (node.nodes[idx] == null) {
+                node.nodes[idx] = new TrieNode();
+            }
+            node = node.nodes[idx];
+        }
+        node.end = true;
     }
 
     public boolean search(String word) {
-        return false;
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if (node.nodes[idx] == null) {
+                return false;
+            }
+            node = node.nodes[idx];
+        }
+        return node.end;
     }
 
     public boolean startsWith(String prefix) {
-        return false;
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            int idx = c - 'a';
+            if (node.nodes[idx] == null) {
+                return false;
+            }
+            node = node.nodes[idx];
+        }
+        return true;
     }
 
 }
