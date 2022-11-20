7515d5ebb41e1d9689d818dc7978ff81fc3d3514
wengru <573519534@qq.com>
2022-11-20 23:45:23 +0800

    feat: 128新增题解，200补充并查集题解

--- src/LeetCode128.java
+++ src/LeetCode128.java
@@ -1,13 +1,122 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class LeetCode128 {
 
+    // 朴素哈希
+    // 把寻找长度的任务交给同一序列中最小的元素
+//    public int longestConsecutive(int[] nums) {
+//        if (nums == null || nums.length == 0) {
+//            return 0;
+//        }
+//        int result = 1;
+//        Map<Integer, Object> parentMap = new HashMap<>();
+//        for (int i=0; i<nums.length; i++) {
+//            parentMap.put(nums[i], new Object());
+//        }
+//        for (int i=0; i<nums.length; i++) {
+//            if (!parentMap.containsKey(nums[i]-1)) {
+//                int count = 0;
+//                while (parentMap.containsKey(nums[i]+(++count)));
+//                result = result > count ? result : count;
+//            }
+//        }
+//        return result;
+//    }
+
+    // 改良版哈希，与朴素哈希相同，哈希表value记录的是右边界
+    // ...
+
+    // 动态规划
     public int longestConsecutive(int[] nums) {
-        return 1;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        int result = 1;
+        for (int n : nums) {
+            if (!map.containsKey(n)) {
+                int left = map.getOrDefault(n - 1, 0);
+                int right = map.getOrDefault(n + 1, 0);
+                int length = left + right + 1;
+                result = Math.max(length, result);
+                map.put(n, -1);
+                map.put(n - left, length);
+                map.put(n + right, length);
+            }
+        }
+        return result;
     }
+
+//    private Map<Integer, Integer> parents;
+//
+//    private Map<Integer, Integer> rank;
+//
+//    // 以key为根节点下的节点的数量
+//    private Map<Integer, Integer> count;
+//
+//    public void init(int[] nums) {
+//        parents = new HashMap<>();
+//        rank = new HashMap<>();
+//        count = new HashMap<>();
+//        for (int n : nums) {
+//            parents.put(n, n);
+//            rank.put(n, 0);
+//            count.put(n, 1);
+//        }
+//    }
+//
+//    public int find(int n) {
+//        // 并查集核心代码
+//        if (parents.get(n) == n) {
+//            return n;
+//        } else {
+//            parents.put(n, find(parents.get(n)));
+//            return parents.get(n);
+//        }
+//    }
+//
+//    // 按秩合并，秩小的树作为子树合并到秩大的树里面
+//    public int union(int x, int y) {
+//        int rootx = find(x);
+//        int rooty = find(y);
+//        if (rootx != rooty) {
+//            if (rank.get(rootx) < rank.get(rooty)) {
+//                parents.put(rootx, rooty);
+//            } else if (rank.get(rootx) > rank.get(rooty)) {
+//                parents.put(rooty, rootx);
+//            } else {
+//                parents.put(rootx, rooty);
+//                rank.put(rooty, rank.get(rootx) + 1);
+//            }
+//            count.put(rank.get(rootx) > rank.get(rooty) ? rootx : rooty, count.get(rootx) + count.get(rooty));
+//        }
+//        return count.get(rank.get(rootx) > rank.get(rooty) ? rootx : rooty);
+//    }
+//
+//    // 并查集
+//    public int longestConsecutive(int[] nums) {
+//        if (nums == null || nums.length == 0) {
+//            return 0;
+//        }
+//        int result = 1;
+//        init(nums);
+//        // key是当前节点，value是父节点
+//        // 向左合并
+//        for (int n : nums) {
+//            if (parents.containsKey(n - 1)) {
+//                int k = union(n - 1, n);
+//                result = Math.max(result, union(n - 1, n));
+//            }
+//        }
+//        return result;
+//    }
 
     public static void main(String[] args) {
         LeetCode128 l = new LeetCode128();
         System.out.println(l.longestConsecutive(new int[]{100,4,200,1,3,2})); // 4
         System.out.println(l.longestConsecutive(new int[]{0,3,7,2,5,8,4,6,0,1})); // 9
+        System.out.println(l.longestConsecutive(new int[]{0})); // 1
     }
 
 }
--- src/LeetCode200.java
+++ src/LeetCode200.java
@@ -1,49 +1,126 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeetCode200 {
 
-    public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) {
-            return 0;
-        }
-        int sum = 0;
-        for (int i=0; i<grid.length; ++i) {
-            for (int j=0; j<grid[i].length; ++j) {
+//    public int numIslands(char[][] grid) {
+//        if (grid == null || grid.length == 0) {
+//            return 0;
+//        }
+//        int sum = 0;
+//        for (int i=0; i<grid.length; ++i) {
+//            for (int j=0; j<grid[i].length; ++j) {
+//                if (grid[i][j] == '1') {
+//                    sum++;
+//                    dfs(grid, i, j);
+//                }
+//            }
+//        }
+//        return sum;
+//    }
+//
+//    void dfs(char[][] grid, int y, int x) {
+//        if (y >=0 && y < grid.length && x >=0 && x < grid[y].length && grid[y][x] == '1') {
+//            grid[y][x] = '0';
+//            dfs(grid, y+1, x);
+//            dfs(grid, y-1, x);
+//            dfs(grid, y, x+1);
+//            dfs(grid, y, x-1);
+//        }
+//    }
+
+    private int[] parents;
+
+    private int[] rank;
+
+    // 岛屿的数量
+    private int count;
+
+    public void init(char[][] grid) {
+        count = 0;
+        parents = new int[grid.length*grid[0].length];
+        rank = new int[grid.length*grid[0].length];
+        for (int i=0; i<grid.length; i++) {
+            for (int j=0; j<grid[0].length; j++) {
                 if (grid[i][j] == '1') {
-                    sum++;
-                    dfs(grid, i, j);
+                    int idx = i * grid[0].length + j;
+                    parents[idx] = idx;
+                    rank[idx] = 0;
+                    count++;
                 }
             }
         }
-        return sum;
     }
 
-    void dfs(char[][] grid, int y, int x) {
-        if (y >=0 && y < grid.length && x >=0 && x < grid[y].length && grid[y][x] == '1') {
-            grid[y][x] = '0';
-            dfs(grid, y+1, x);
-            dfs(grid, y-1, x);
-            dfs(grid, y, x+1);
-            dfs(grid, y, x-1);
+    // 查找传入节点的根节点，查找途中路径压缩
+    public int find(int x) {
+        // 并查集核心代码
+        return x == parents[x] ? x : (parents[x] = find(parents[x]));
+    }
+
+    // 传入两个结点，按秩合并，秩小的作为子数合并到秩大的树中去
+    public void union(int x, int y) {
+        int rootx = find(x);
+        int rooty = find(y);
+        if (rootx != rooty) {
+            if (rank[rootx] < rank[rooty]) {
+                parents[rootx] = rooty;
+            } else if (rank[rootx] > rooty) {
+                parents[rooty] = rootx;
+            } else {
+                // 两棵树的秩相同
+                parents[rootx] = rooty;
+                rank[rooty]++;
+            }
+            count--; // 两个岛屿合并完之后，数量-1
         }
+    }
+
+    // union find
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int rowLength = grid[0].length;
+        init(grid);
+        // 只要发现相邻的两块陆地就执行一次合并操作
+        // 从左上角开始，按照右下的方向遍历，遍历过的地方置为'0'
+        for (int i=0; i<grid.length; i++) {
+            for (int j=0; j<grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    grid[i][j] = '0';
+                    // 合并右
+                    if (j != grid[0].length - 1 && grid[i][j+1] == '1') {
+                        union(i*rowLength + j, i*rowLength + j + 1);
+                    }
+                    // 合并下
+                    if (i != grid.length - 1 && grid[i+1][j] == '1') {
+                        union(i*rowLength + j, (i + 1)*rowLength + j);
+                    }
+                }
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
         LeetCode200 l = new LeetCode200();
-//        System.out.println(l.numIslands( // 1
-//                new char[][] {
-//                        new char[]{'1', '1', '1', '1', '0'},
-//                        new char[]{'1', '1', '0', '1', '0'},
-//                        new char[]{'1', '1', '0', '0', '0'},
-//                        new char[]{'0', '0', '0', '0', '0'},
-//                }
-//        ));
-//        System.out.println(l.numIslands( // 3
-//                new char[][] {
-//                        new char[]{'1', '1', '0', '0', '0'},
-//                        new char[]{'1', '1', '0', '0', '0'},
-//                        new char[]{'0', '0', '1', '0', '0'},
-//                        new char[]{'0', '0', '0', '1', '1'},
-//                }
-//        ));
+        System.out.println(l.numIslands( // 1
+                new char[][] {
+                        new char[]{'1', '1', '1', '1', '0'},
+                        new char[]{'1', '1', '0', '1', '0'},
+                        new char[]{'1', '1', '0', '0', '0'},
+                        new char[]{'0', '0', '0', '0', '0'},
+                }
+        ));
+        System.out.println(l.numIslands( // 3
+                new char[][] {
+                        new char[]{'1', '1', '0', '0', '0'},
+                        new char[]{'1', '1', '0', '0', '0'},
+                        new char[]{'0', '0', '1', '0', '0'},
+                        new char[]{'0', '0', '0', '1', '1'},
+                }
+        ));
         System.out.println(l.numIslands( // 1
                 new char[][] {
                         new char[]{'1', '1', '1'},
