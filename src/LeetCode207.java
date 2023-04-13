e1fa68223b40030be650fc684a87698a85633d7c
wengru <573519534@qq.com>
2023-04-13 23:41:30 +0800

    feat: 补充题解207

--- src/LeetCode207.java
+++ src/LeetCode207.java
@@ -1,30 +1,113 @@
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
+
 public class LeetCode207 {
 
+    // BFS 时间复杂度O(M + N) 空间复杂度O(M + N) M: 结点数, N: 邻边数量
+//    public boolean canFinish(int numCourses, int[][] prerequisites) {
+//        if (numCourses <= 0 || prerequisites == null) {
+//            return false;
+//        }
+////        if (prerequisites.length == 0) {
+////            return true;
+////        }
+//        int[] inDegree = new int[numCourses];
+//        Set<Integer>[] adjacent = new Set[numCourses];
+//        for (int i = 0; i < adjacent.length; i++) {
+//            adjacent[i] = new HashSet<>();
+//        }
+//        for (int[] p : prerequisites) {
+//            inDegree[p[0]]++;
+//            adjacent[p[1]].add(p[0]);
+//        }
+//        Queue<Integer> queue = new ArrayDeque<>();
+//        for (int i = 0; i < inDegree.length; i++) {
+//            if (inDegree[i] == 0) {
+//                queue.offer(i);
+//            }
+//        }
+//        int count = 0;
+//        while (!queue.isEmpty()) {
+//            // 已经在队列里的结点入度都为0
+//            int node = queue.poll();
+//            count++;
+//            for (int adjNode : adjacent[node]) {
+//                inDegree[adjNode]--;
+//                if (inDegree[adjNode] == 0) {
+//                    queue.add(adjNode);
+//                }
+//            }
+//        }
+//        return count == numCourses;
+//    }
+
+    // DFS 思路是判断是否存在环，时间复杂度O(M + N) 空间复杂度O(M + N) M: 结点数, N: 邻边数量
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (numCourses <= 0 || prerequisites == null) {
+            return false;
+        }
+//        if (prerequisites.length == 0) {
+//            return true;
+//        }
+        // 0 未遍历 1遍历中 2遍历完成
+        int[] marked = new int[numCourses];
+        Set<Integer>[] adjacent = new Set[numCourses];
+        for (int i = 0; i < adjacent.length; i++) {
+            adjacent[i] = new HashSet<>();
+        }
+        for (int[] p : prerequisites) {
+            adjacent[p[0]].add(p[1]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (dfs(marked, adjacent, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 判断是否存在环
+    public boolean dfs(int[] marked, Set<Integer>[] adjacent, int node) {
+        // 开始遍历，当前节点的mark置为1
+        if (marked[node] == 2) {
+            return false;
+        }
+        if (marked[node] == 1) {
+            return true;
+        }
+        // 开始遍历，状态设置为遍历中
+        marked[node] = 1;
+        // 深度遍历依赖的结点
+        for (Integer preNode : adjacent[node]) {
+            if (dfs(marked, adjacent, preNode)) {
+                return true;
+            }
+        }
+        marked[node] = 2;
         return false;
     }
 
     public static void main(String[] args) {
         LeetCode207 l = new LeetCode207();
-        l.canFinish(
+        System.out.println(l.canFinish(
                 2,
                 new int[][]{
                         new int[]{1,0}
                 }
-        ); // [0,1]
-        l.canFinish(
+        )); // true
+        System.out.println(l.canFinish(
                 2,
                 new int[][]{
                         new int[]{1,0},
-                        new int[]{2,0},
-                        new int[]{3,1},
-                        new int[]{3,2}
+                        new int[]{0,1}
                 }
-        ); // [0,2,1,3]
-        l.canFinish(
+        )); // false
+        System.out.println(l.canFinish(
                 1,
                 new int[][]{}
-        ); // [0]
+        )); // true
     }
 
 }
---
+++ src/LeetCode402.java
@@ -0,0 +1,13 @@
+public class LeetCode402 {
+
+    public String removeKdigits(String num, int k) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        LeetCode402 l = new LeetCode402();
+        System.out.println(l.removeKdigits("1432219", 3)); // "1219"
+        System.out.println(l.removeKdigits("10200", 1)); // "200"
+    }
+
+}
